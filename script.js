726edd1744a1892074bee45e835c46aaedb541f0
camillemilliminute <camille.melinjulien@hotmail.com>
2026-05-20 23:50:25 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+alert();
+eval();
+window.alert();
+document.querySelector('div');
+eval();
+document;
+eval();
+eval("3 + 5");
+fot (;;) {
+  alert("ffvjhrfghjffcbhfcvhfcvjhffvhtchhgvhhfvhhfvjfcbhfcvggcchhgbhh reste");
+}
